9e79e9ce3f4323e461a484a2278ccd6b065d2687
sans-creator <sanskarj163@gmail.com>
2025-07-28 13:19:37 +0530

    homePage

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <AiTools/>
+    <Plan/>
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default Home
